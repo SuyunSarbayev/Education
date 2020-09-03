4d5b4981f3a533da54a20ce4f57f922597ff7479
suyun <sarbayev.s@chocofood.kz>
2020-09-03 20:12:13 +0600

    Added group holder

--- app/src/main/java/suyun/personal/education/presentation/adapter/StudentsAdapter.kt
+++ app/src/main/java/suyun/personal/education/presentation/adapter/StudentsAdapter.kt
@@ -2,25 +2,49 @@ package suyun.personal.education.presentation.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import suyun.personal.education.R
 import suyun.personal.education.data.Student
+import suyun.personal.education.presentation.holders.GroupHolder
 import suyun.personal.education.presentation.holders.StudentsHolder
 
 // Assembly line (Конвеер)
-class StudentsAdapter(var students: ArrayList<Student>) : RecyclerView.Adapter<StudentsHolder>() {
+class StudentsAdapter(var students: ArrayList<Student>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentsHolder {
+    var STUDENT_TYPE = 1
+    var GROUP_TYPE = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         Log.d("RecyclerView", "onCreateViewHolder")
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.viewholder_student, parent, false)
-        return StudentsHolder(view)
+        var view: View? = null
+        when(viewType){
+            GROUP_TYPE -> {
+                view = LayoutInflater.from(parent.context).inflate(R.layout.viewholder_student, parent, false)
+                GroupHolder(view)
+            }
+            else -> {
+                view = LayoutInflater.from(parent.context).inflate(R.layout.viewholder_student, parent, false)
+                StudentsHolder(view)
+            }
+        }
     }
 
     override fun getItemCount(): Int { return students?.size }
 
-    override fun onBindViewHolder(holder: StudentsHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder , position: Int) {
         Log.d("RecyclerView", "onBindViewHolder")
-        holder.bind(students.get(position))
+        when(position%2){
+            0 -> {(holder as GroupHolder)}
+            1 -> {(holder as StudentsHolder).bind(students.get(position))}
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when(position%2){
+            0 -> GROUP_TYPE
+            else -> STUDENT_TYPE
+        }
     }
 }
---
+++ app/src/main/java/suyun/personal/education/presentation/holders/GroupHolder.kt
@@ -0,0 +1,9 @@
+package suyun.personal.education.presentation.holders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import suyun.personal.education.presentation.contracts.StudentsFragmentContract
+
+class GroupHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
